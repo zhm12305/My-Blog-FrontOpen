6b3667b4125cbe36fa7bf436edf661ad438ff898
zhm12305 <1782924062@qq.com>
2025-09-12 09:55:09 +0800

    1

--- src/utils/title.js
+++ src/utils/title.js
@@ -1,4 +1,4 @@
-let titleTime, OriginTitile = document.title;
+let titleTime, OriginTitile = "羌笛萧萧安然の窝";
 document.addEventListener("visibilitychange", (function () {
   document.hidden ? (document.title = "w(ﾟДﾟ)w 不要走！再看看嘛！😭", clearTimeout(titleTime)) : (document.title = "♪(^∇^*)欢迎肥来！🥰", titleTime = setTimeout((function () {
     document.title = OriginTitile
